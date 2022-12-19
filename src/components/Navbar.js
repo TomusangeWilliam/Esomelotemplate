6c025b535255b40d2bdfb1f15135f49a0bd37aa0
tomusangewilliam <william.tomusange@esomelo.com>
2022-12-19 15:50:00 +0300

    icons on the Mobile version

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { useScrollYPosition } from "react-use-scroll-position";
+import { faHome, faUser, faBurn, faUserAstronaut, faLitecoinSign,fa9, faUmbrella, faChessQueen, faBook, faMoneyBill, faServer, faTasks} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import "../assets/navbar.css";
 
 function Navbar({ links }) {
@@ -18,20 +20,23 @@ function Navbar({ links }) {
       <a href="https://www.esomelo.com/" >
           <img src="./images/esomelologo.png" id="logoimg"  alt="download" />
        </a>
-          
-          
         <div onclick="window.open('newurl.html','mywindow');" className="nav-logo" >ESOMELO</div>
         
-
-
         <nav className="nav-links__container">
           {links &&
             links.map((link, i) => (
-              <a className="nav-link" href={link.href} key={i}>
-                
-                <div className="nav-link__text">{link.title}</div>
-                <div className="nav-link__background" />
-              </a>
+                              <a className="nav-link" href={link.href} key={i}>
+                      {/* Display the corresponding icon based on the link title */}
+                      {link.title === "Home" && <FontAwesomeIcon icon={faHome} />}
+                      {link.title === "Features" && <FontAwesomeIcon icon={faTasks} />}
+                      {link.title === "Services" && <FontAwesomeIcon icon={faServer} />}
+                      {link.title === "Pricing" && <FontAwesomeIcon icon={faMoneyBill} />}
+                      {link.title === "Blog" && <FontAwesomeIcon icon={faBook} />}
+                      {link.title === "Customers" && <FontAwesomeIcon icon={faChessQueen} />}
+                      {link.title === "Login" && <FontAwesomeIcon icon={faUser} />}
+                      <div className="nav-link__text">{link.title}</div>
+                      <div className="nav-link__background" />
+                    </a>
             ))}
         </nav>
 
@@ -48,6 +53,7 @@ function Navbar({ links }) {
 
 Navbar.defaultProps = {
   links: [
+
     { title: "Home", href: "#home" },
     { title: "Features", href: "#features" },
     { title: "Services", href: "#services" },
